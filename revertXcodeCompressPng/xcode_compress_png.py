a22419630e2b8c34281ec71272df34abfc8b6f6c
fxl <wormlucky@gmail.com>
2014-03-20 03:26:38 +0800

    over

---
+++ revertXcodeCompressPng/xcode_compress_png.py
@@ -0,0 +1,36 @@
+# coding=utf-8
+# revert_xcode_compress_png.py
+# @athor fxl
+# @date 2014-03-20
+
+from optparse import OptionParser
+
+import os,shutil
+
+__help = '''usage %prog [options]'''
+__parser = OptionParser(__help)
+__parser.add_option('-i','--input',dest = 'input',action='store',default='input')
+__parser.add_option('-o','--output',dest ='output',action='store',default='output')
+__parser.add_option('-r','--revert',dest ='revert',action='store',default='true')
+
+
+def dealImgInDir(dir,outputdir):
+
+    if not os.path.exists(outputdir):
+        print("out is " + outputdir)
+        os.makedirs(outputdir)
+    if os.path.exists(dir):
+        for d in os.listdir(dir):
+            childpath = os.path.join(dir,d)
+            out = os.path.join(outputdir,d)
+            if os.path.isdir(d):
+                dealImgInDir(childpath,out)
+            else:
+                if os.path.splitext(childpath)[1]==".png":
+                    cmd = "xcrun -sdk iphoneos pngcrush -revert-iphone-optimizations -q "+childpath+" " +os.path.join(outputdir,d)
+                    os.system(cmd)
+            
+if __name__=='__main__':
+    options,args = __parser.parse_args()
+    basedir = os.path.split(os.path.realpath(__file__))[0]
+    dealImgInDir(os.path.join(basedir,options.input),os.path.join(basedir,options.output))
