89dadd6314efbcd93fa2c5e425f2b32b39c3f6b5
范鑫磊 <xinlei.fan@redatoms.com>
2014-03-21 19:02:21 +0800

    bug fix: if there have other image resource like jpg we should copy them

--- revertXcodeCompressPng/xcode_compress_png.py
+++ revertXcodeCompressPng/xcode_compress_png.py
@@ -15,21 +15,23 @@ __parser.add_option('-r','--revert',dest ='revert',action='store',default='true'
 
 
 def dealImgInDir(dir,outputdir,isRevert):
-
-    if not os.path.exists(outputdir):
-        print("out is " + outputdir)
-        os.makedirs(outputdir)
     if os.path.exists(dir):
         for d in os.listdir(dir):
             childpath = os.path.join(dir,d)
             out = os.path.join(outputdir,d)
-            if os.path.isdir(d):
+            if os.path.isdir(childpath):
                 dealImgInDir(childpath,out,isRevert)
             else:
-                if os.path.splitext(childpath)[1]==".png":
+                ext = os.path.splitext(d)[1]
+                if ext == ".png" or ext == ".jpg":
+                    if not os.path.exists(outputdir):
+                        os.makedirs(outputdir)    
+                if ext == ".png":
                     revertopt = ' -revert-iphone-optimizations' if isRevert else ''
                     cmd       = "xcrun -sdk iphoneos pngcrush"+revertopt+" -q "+childpath+" " +os.path.join(outputdir,d)
                     os.system(cmd)
+                elif ext == ".jpg":
+                    os.system("cp " + childpath + " " + os.path.join(outputdir,d))
             
 if __name__=='__main__':
     options,args = __parser.parse_args()
